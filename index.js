229fedd9d53ae3149e4850e5e6d8e5e3b6d33c3a
Nick Hynes <nhynes@mit.edu>
2015-12-15 18:17:44 -0500

    Improve debug logging

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ var attach = require('neovim-client'),
 
 // redirect stderr and set up debug output
 if ( process.env.NEOVIM_JS_DEBUG ) {
-    debugOut = fs.createWriteStream( process.env.NEOVIM_JS_DEBUG, { flags: 'w' } )
+    debugOut = fs.createWriteStream( process.env.NEOVIM_JS_DEBUG, { flags: 'a' } )
     process.stderr.write = debug.write
     debug = function() {
         var args = [].slice.call( arguments ),
-            sout = fmt.apply( null, [].concat( 'node-host:', args ) )
+            sout = fmt.apply( null, [].concat( Date(), 'node-host:', args ) )
         debugOut.write(sout + '\n')
     }
 }
