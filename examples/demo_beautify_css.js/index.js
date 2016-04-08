c4286d23df7125dd10e0bae0864c2d73b99dd90c
Nick Hynes <nhynes@mit.edu>
2016-04-08 06:51:23 -0400

    Update BeautifyCSS example to use async API

--- examples/demo_beautify_css.js/index.js
+++ examples/demo_beautify_css.js/index.js
@@ -2,11 +2,11 @@
 
 let beautify = require('cssbeautify')
 
-plugin.commandSync('BeautifyCSS', nvim => {
+plugin.commandSync('BeautifyCSS', ( nvim, cb ) => {
     nvim.getCurrentBuffer( ( err, buf ) => {
         buf.getLineSlice( 0, -1, true, true, ( err, lines ) => {
             let beautified = beautify( lines.join('\n') )
-            buf.setLineSlice( 0, -1, true, true, beautified.split('\n') )
+            buf.setLineSlice( 0, -1, true, true, beautified.split('\n'), cb )
         })
     })
 })
